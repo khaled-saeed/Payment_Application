26b26a0ba2368798dd1653b0c4ec1bfacda78d84
3817459 <khaled.saeed.tawfik@gmail.com>
2022-08-20 19:30:06 +0200

    final commit

--- Terminal/terminal.c
+++ Terminal/terminal.c
@@ -2,12 +2,11 @@
 #include<stdio.h>
 #include<ctype.h>
 #include<string.h>
-/*********************** try to use date from the OS ******************/ 
-EN_terminalError_t getTransactionDate(ST_terminalData_t *termData)
+EN_terminalError_t getTransactionDate(ST_terminalData_t *termData)  //dd/mm/yyyy
 {
-    printf("Please Enter the transaction data : ");
+    printf("Please Enter the transaction date : ");
     char date[11] = "\0"; 
-    scanf("%11s",date) ; 
+    scanf(" %11s",date) ; 
     int len = strlen(date);
     strcpy(termData->transactionDate , date); 
     //printf("len: %d\n",len);                  // for debugging purposes 
@@ -15,32 +14,37 @@ EN_terminalError_t getTransactionDate(ST_terminalData_t *termData)
 
     if(*termData->transactionDate == '\0'|| len <10 || !checkDateFormat(date,len))
     {
-        return WRONG_NAME ;
+        //printf("WRONG_DATE\n");
+        return WRONG_DATE ;
     }
     else
     {
+        //printf("TERMINAL_OK\n");
         return TERMINAL_OK ;
     }
 }
-EN_terminalError_t isCardExpired(ST_cardData_t *cardData, ST_terminalData_t *termData)
+EN_terminalError_t isCardExpired(ST_cardData_t *cardData, ST_terminalData_t *termData) 
 {
     int EX_date_month = (cardData->cardExpirationDate[0]-'0') * 10 + (cardData->cardExpirationDate[1] -'0'); 
     int EX_date_year = (cardData->cardExpirationDate[3]-'0') * 10 + (cardData->cardExpirationDate[4]-'0');
     int Curr_date_month = (termData->transactionDate[3]-'0') * 10 + (termData->transactionDate[4] -'0'); 
     int Curr_date_year = (termData->transactionDate[8]-'0' )* 10 + (termData->transactionDate[9] -'0'); 
-    printf("Expiration date : %d/%d\n",EX_date_month,EX_date_year);         // Foe debugging purposes
-    printf("current date : %d/%d",Curr_date_month,Curr_date_year);          // Foe debugging purposes
+    //printf("Expiration date : %d/%d\n",EX_date_month,EX_date_year);         // For debugging purposes
+    //printf("current date : %d/%d\n",Curr_date_month,Curr_date_year);          // For debugging purposes
     if(EX_date_year < Curr_date_year)
     {
+      //  printf("EXPIRED_CARD\n");
         return EXPIRED_CARD ; 
     }
     else if (EX_date_year == Curr_date_year)
     {
         if(EX_date_month < Curr_date_month)
         {
+        //    printf("EXPIRED_CARD\n");
             return EXPIRED_CARD ; 
         }
     }
+    //printf("TERMINAL_OK\n");
     return TERMINAL_OK ; 
 }
 EN_terminalError_t isValidCardPAN(ST_cardData_t *cardData)
@@ -51,32 +55,38 @@ EN_terminalError_t getTransactionAmount(ST_terminalData_t *termData)
 {
     printf("Please Enter the  transaction amount :") ;
     float amount ; 
-    scanf("%f",&amount); 
+    scanf(" %f",&amount); 
     termData->transAmount = amount ; 
     if(termData->transAmount <= 0)
     {
+      //  printf("INVALID_AMOUNT\n");                                   // For debugging purposes
         return INVALID_AMOUNT ; 
     }
+    //printf("TERMINAL_OK\n");
     return TERMINAL_OK; 
 }
 EN_terminalError_t isBelowMaxAmount(ST_terminalData_t *termData)
 {
     if(termData->transAmount > termData->maxTransAmount )
     {
+        //printf("EXCEED_MAX_AMOUNT\n");                                 // For debugging purposes
         return EXCEED_MAX_AMOUNT ; 
     }
+    //printf("TERMINAL_OK\n");
     return TERMINAL_OK ; 
 }
 EN_terminalError_t setMaxAmount(ST_terminalData_t *termData)
 {
-    // printf( "Enter the maximum amount for transaction :");
-    // float max ; 
-    // scanf("%d",&max); 
+    //printf( "Enter the maximum amount for transaction :");
+    //float max ; 
+    //scanf(" %d",&max); 
     termData->maxTransAmount = 5000 ; 
     if(termData->maxTransAmount <= 0)
     {
+        printf("INVALID_MAX_AMOUNT\n");
         return INVALID_MAX_AMOUNT ; 
     }
+    //printf("TERMINAL_OK\n");
     return TERMINAL_OK ; 
 }
 bool checkDateFormat(char * date, int len)
@@ -90,7 +100,7 @@ bool checkDateFormat(char * date, int len)
                 i++; 
             else
             {
-                //printf("false at %d at %c",i , date[i]);      // Foe debugging purposes
+                //printf("false at %d at %c",i , date[i]);      // For debugging purposes
                 return false ; 
             }   
         }
@@ -100,7 +110,7 @@ bool checkDateFormat(char * date, int len)
                 i++; 
             else
             {
-                //printf("false at %d at %c",i , date[i]);      // Foe debugging purposes
+                //printf("false at %d at %c",i , date[i]);      // For debugging purposes
                 return false ; 
             }  
         }
